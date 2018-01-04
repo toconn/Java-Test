e2bf2506f6767e2324ad08a23f581f525651b67f
Tim OConnell <tuc.github@uaconaill.com>
2018-01-04 16:17:11 -0500

    refactored PersonFactory.

--- Source/Main/Java/ua/test/entities/PersonFactory.java
+++ Source/Main/Java/ua/test/entities/PersonFactory.java
@@ -16,15 +16,15 @@ public class PersonFactory {
 		personMap = new HashMap<>();
 		
 		personMap.put ("John",		new Person("John",		"Murphy",		"M", 30));
-		personMap.put ("Sarah",		new Person("Sarah",		"Lancaster",	"F", 17));
-		personMap.put ("Frank",		new Person("Frank",		"Bomer",		"M", 54));
-		personMap.put ("Martha",	new Person("Martha",	"Casey",		"F", 34));
+		personMap.put ("Sarah",		new Person("Sarah",		"Lancaster",		"F", 17));
+		personMap.put ("Frank",		new Person("Frank",		"Bomer",			"M", 54));
+		personMap.put ("Martha",		new Person("Martha",		"Casey",			"F", 34));
 		personMap.put ("Jeff",		new Person("Jeff",		"Shaw",			"M", 25));
-		personMap.put ("Yvonna",	new Person("Yvonna",	"Cohan",		"F", 28));
+		personMap.put ("Yvonna",		new Person("Yvonna",		"Cohan",			"F", 28));
 		personMap.put ("Tony",		new Person("Tony",		"McArthur",		"M", 71));
 		personMap.put ("Jane",		new Person("Jane",		"Decker",		"F", 12));
 		personMap.put ("Hugh",		new Person("Hugh",		"Decker",		"M", 22));
-		personMap.put ("Anna",		new Person("Anna",		"Wu", "F", 45));
+		personMap.put ("Anna",		new Person("Anna",		"Wu", 			"F", 45));
 		
 		return personMap;
 	}
@@ -36,15 +36,15 @@ public class PersonFactory {
 		persons = new ArrayList<>();
 		
 		persons.add (new Person("John",		"Murphy",		"M", 30));
-		persons.add (new Person("Sarah",	"Lancaster",	"F", 17));
-		persons.add (new Person("Frank",	"Bomer",		"M", 54));
-		persons.add (new Person("Martha",	"Casey",		"F", 34));
+		persons.add (new Person("Sarah",		"Lancaster",		"F", 17));
+		persons.add (new Person("Frank",		"Bomer",			"M", 54));
+		persons.add (new Person("Martha",	"Casey",			"F", 34));
 		persons.add (new Person("Jeff",		"Shaw",			"M", 25));
-		persons.add (new Person("Yvonna",	"Cohan",		"F", 28));
+		persons.add (new Person("Yvonna",	"Cohan",			"F", 28));
 		persons.add (new Person("Tony",		"McArthur",		"M", 71));
 		persons.add (new Person("Jane",		"Decker",		"F", 12));
 		persons.add (new Person("Hugh",		"Decker",		"M", 22));
-		persons.add (new Person("Anna",		"Wu", "F", 45));
+		persons.add (new Person("Anna",		"Wu", 			"F", 45));
 
 		return persons;
 	}
@@ -56,9 +56,9 @@ public class PersonFactory {
 		persons = new ArrayList<>();
 		
 		persons.add (new Person("John",		"Murphy",		"M", 30));
-		persons.add (new Person("Sarah",	"Lancaster",	"F", 17));
-		persons.add (new Person("Frank",	"Bomer",		"M", 54));
-		persons.add (new Person("Martha",	"Casey",		"F", 34));
+		persons.add (new Person("Sarah",		"Lancaster",		"F", 17));
+		persons.add (new Person("Frank",		"Bomer",			"M", 54));
+		persons.add (new Person("Martha",	"Casey",			"F", 34));
 		persons.add (new Person("Jeff",		"Shaw",			"M", 25));
 
 		return persons;
@@ -70,11 +70,11 @@ public class PersonFactory {
 		
 		persons = new ArrayList<>();
 		
-		persons.add (new Person("Yvonna",	"Cohan",		"F", 28));
+		persons.add (new Person("Yvonna",	"Cohan",			"F", 28));
 		persons.add (new Person("Tony",		"McArthur",		"M", 71));
 		persons.add (new Person("Jane",		"Decker",		"F", 12));
 		persons.add (new Person("Hugh",		"Decker",		"M", 22));
-		persons.add (new Person("Anna",		"Wu", "F", 45));
+		persons.add (new Person("Anna",		"Wu", 			"F", 45));
 
 		return persons;
 	}
@@ -86,15 +86,15 @@ public class PersonFactory {
 		persons = new HashSet<>();
 		
 		persons.add (new Person("John",		"Murphy",		"M", 30));
-		persons.add (new Person("Sarah",	"Lancaster",	"F", 17));
-		persons.add (new Person("Frank",	"Bomer",		"M", 54));
-		persons.add (new Person("Martha",	"Casey",		"F", 34));
+		persons.add (new Person("Sarah",		"Lancaster",		"F", 17));
+		persons.add (new Person("Frank",		"Bomer",			"M", 54));
+		persons.add (new Person("Martha",	"Casey",			"F", 34));
 		persons.add (new Person("Jeff",		"Shaw",			"M", 25));
-		persons.add (new Person("Yvonna",	"Cohan",		"F", 28));
+		persons.add (new Person("Yvonna",	"Cohan",			"F", 28));
 		persons.add (new Person("Tony",		"McArthur",		"M", 71));
 		persons.add (new Person("Jane",		"Decker",		"F", 12));
 		persons.add (new Person("Hugh",		"Decker",		"M", 22));
-		persons.add (new Person("Anna",		"Wu", "F", 45));
+		persons.add (new Person("Anna",		"Wu", 			"F", 45));
 
 		return persons;
 	}
